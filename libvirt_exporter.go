f7561c978087f8041bcfd890dc7dc9cbc9fdcf81
VincentK-Titandc <55184018+VincentK-Titandc@users.noreply.github.com>
2021-02-02 13:52:24 +0100

    Another try to infer actual amount of memory available & unused.

--- libvirt_exporter.go
+++ libvirt_exporter.go
@@ -314,9 +314,9 @@ func (e *LibvirtExporter) CollectDomain(ch chan<- prometheus.Metric, domain *lib
 	memMax := uint64(0)
 	memStat, err := domain.MemoryStats(11, 0)
 	for _, buf := range memStat {
-		if buf.Tag == int32(libvirt.DOMAIN_MEMORY_STAT_UNUSED) {
+		if buf.Tag == int32(libvirt.DOMAIN_MEMORY_STAT_USABLE) {
 			memUnused = buf.Val
-		} else if buf.Tag == int32(libvirt.DOMAIN_MEMORY_STAT_RSS) {
+		} else if buf.Tag == int32(libvirt.DOMAIN_MEMORY_STAT_AVAILABLE) {
 			memMax = buf.Val
 		}
 	}
